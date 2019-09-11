4c5fbe3fe2432b6d5ceb752cbeda156661c7458c
Long Gong (gtuser) <gonglong@gatech.edu>
2019-09-11 13:34:07 -0400

    removed unnecessary files

--- cmake-build-vcpkg/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Data/GitHub/forked-minisketch/test
-# Build directory: /Data/GitHub/forked-minisketch/cmake-build-vcpkg/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(AllTests "minisketch_test" "COMMAND" "$<TARGET_FILE:minisketch_test>")
-set_tests_properties(AllTests PROPERTIES  _BACKTRACE_TRIPLES "/Data/GitHub/forked-minisketch/test/CMakeLists.txt;19;add_test;/Data/GitHub/forked-minisketch/test/CMakeLists.txt;0;")
